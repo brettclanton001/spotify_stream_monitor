fa31c6bb93e0ee262b251a1409950fc74e72c8a4
Brett Clanton <brettclanton@pm.me>
2022-09-12 22:11:08 -0400

    whitespace

--- stream_monitor.rb
+++ stream_monitor.rb
@@ -10,40 +10,40 @@ end
 
 # Database Connection
 @conn = PG::Connection.new({
-	host: ENV['PG_HOST'],
-	port: ENV['PG_PORT'],
-	dbname: ENV['PG_DB'],
-	user: ENV['PG_USER'],
-	password: ENV['PG_PASSWORD']
+  host: ENV['PG_HOST'],
+  port: ENV['PG_PORT'],
+  dbname: ENV['PG_DB'],
+  user: ENV['PG_USER'],
+  password: ENV['PG_PASSWORD']
 })
 
 def record_spotify_data(object_type, id, object_name, popularity)
 
-	# data validation
-	raise Exception.new "invalid object_type" unless ['artist', 'track'].include?(object_type)
-	raise Exception.new "invalid object id" unless id =~ /[A-Za-z0-9]{22}/
-	raise Exception.new "invalid popularity" unless (0..100).include?(popularity)
-	
-	@conn.exec("
-		INSERT INTO spotify_data_reports (
-			 uuid,
-			 external_object_type,
-			 external_object_id,
-			 external_object_name,
-			 popularity,
-			 created_at,
-			 updated_at
-		)
-		VALUES (
-			 GEN_RANDOM_UUID(),
-			 '#{object_type}',
-			 '#{id}',
-			 '#{object_name}',
-			 #{popularity},
-			 NOW(),
-			 NOW()
-		);
-	")
+  # data validation
+  raise Exception.new "invalid object_type" unless ['artist', 'track'].include?(object_type)
+  raise Exception.new "invalid object id" unless id =~ /[A-Za-z0-9]{22}/
+  raise Exception.new "invalid popularity" unless (0..100).include?(popularity)
+
+  @conn.exec("
+    INSERT INTO spotify_data_reports (
+      uuid,
+      external_object_type,
+      external_object_id,
+      external_object_name,
+      popularity,
+      created_at,
+      updated_at
+    )
+    VALUES (
+      GEN_RANDOM_UUID(),
+      '#{object_type}',
+      '#{id}',
+      '#{object_name}',
+      #{popularity},
+      NOW(),
+      NOW()
+    );
+  ")
 end
 
 RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
@@ -55,15 +55,15 @@ playlist = RSpotify::Playlist.find_by_id(ENV['SPOTIFY_PLAYLIST_ID'])
 
 playlist.tracks.each do |track|
 
-	track.artists.each do |artist|
-		unless artists.include?(artist.id)
-			artists.append(artist.id)
-			record_spotify_data('artist', artist.id, artist.name, artist.popularity)
-		end
-	end
+  track.artists.each do |artist|
+    unless artists.include?(artist.id)
+      artists.append(artist.id)
+      record_spotify_data('artist', artist.id, artist.name, artist.popularity)
+    end
+  end
 
-	unless tracks.include?(track.id)
-		tracks.append(track.id)
-		record_spotify_data('track', track.id, track.name, track.popularity)
-	end
+  unless tracks.include?(track.id)
+    tracks.append(track.id)
+    record_spotify_data('track', track.id, track.name, track.popularity)
+  end
 end
